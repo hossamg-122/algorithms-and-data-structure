2ea969823dbf937ba5803631459ea3579372ff51
hossamg-122 <hossamg122@gmail.com>
2023-05-13 22:41:26 +0300

    feat: 1st solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let container = {};
+  for (const char of str) {
+    if (container[char]) container[char]++;
+    else {
+      container[char] = 1;
+    }
+  }
+  return Object.keys(container).reduce((prev, curr) => {
+    return container[curr] > container[prev] ? curr : prev;
+  });
+}
 
+// 1st solution
+// function maxChar(str) {
+//     let container = {};
+//     for (const char of str) {
+//       if (container[char]) container[char]++;
+//       else {
+//         container[char] = 1;
+//       }
+//     }
+//     return Object.keys(container).reduce((prev, curr) => {
+//       return container[curr] > container[prev] ? curr : prev;
+//     });
+//   }
 module.exports = maxChar;
