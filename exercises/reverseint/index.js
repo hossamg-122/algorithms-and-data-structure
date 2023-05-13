ce8da28b7e377438b58545357bdcc6aae986eeaa
hossamg-122 <hossamg122@gmail.com>
2023-05-13 17:59:22 +0300

    feat: 1st solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (n > 0) {
+    return Number(String(n).split("").reverse().join(""));
+  } else {
+    const modifiedNumber = String(n).split("");
+    let negativeSign = modifiedNumber.shift();
+    return Number(negativeSign + modifiedNumber.reverse().join(""));
+  }
+}
 
+// 1st solution
+// function reverseInt(n) {
+//     if (n > 0) {
+//       return Number(String(n).split("").reverse().join(""));
+//     } else {
+//       const modifiedNumber = String(n).split("");
+//       let negativeSign = modifiedNumber.shift();
+//       return Number(negativeSign + modifiedNumber.reverse().join(""));
+//     }
+//   }
 module.exports = reverseInt;
