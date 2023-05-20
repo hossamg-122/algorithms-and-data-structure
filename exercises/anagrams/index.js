7905834f8d047c7c29e0dc96044b39367022d273
hossamg-122 <hossamg122@gmail.com>
2023-05-20 23:03:56 +0300

    feat: 1st solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,26 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  if (stringA.length !== stringB.length) return false;
+  const getCharsCount = (array) =>
+    array.split("").reduce((prev, curr) => {
+      if (prev[curr]) {
+        prev[curr] = prev[curr] + 1;
+      } else {
+        prev[curr] = 1;
+      }
+      return prev;
+    }, {});
+  const modifiedStringA = getCharsCount(stringA);
+  const modifiedStringB = getCharsCount(stringB);
+
+  let isAnagram = false;
+
+  for (const key in modifiedStringA) {
+    isAnagram = modifiedStringA[key] === modifiedStringB[key] ? true : false;
+  }
+  return isAnagram;
+}
 
 module.exports = anagrams;
